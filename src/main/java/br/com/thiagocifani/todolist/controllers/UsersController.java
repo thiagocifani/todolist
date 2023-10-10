2d2b275769ffce6fde52ec9768b5d55083234df1
thiagocifani <cifani.thiago@gmail.com>
2023-10-10 14:32:16 -0300

    Add validation to username and return ResponseEntity

--- src/main/java/br/com/thiagocifani/todolist/controllers/UsersController.java
+++ src/main/java/br/com/thiagocifani/todolist/controllers/UsersController.java
@@ -1,11 +1,15 @@
 package br.com.thiagocifani.todolist.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.thiagocifani.todolist.models.User;
+import br.com.thiagocifani.todolist.repositories.IUserRepository;
 
 /**
  * Modificador
@@ -17,21 +21,17 @@ import br.com.thiagocifani.todolist.models.User;
 @RequestMapping("/users")
 public class UsersController {
 
-  /**
-   * String
-   * Integer
-   * Double
-   * Float
-   * char
-   * Date
-   * void
-   */
-  /*
-   * Body
-   */
-  @PostMapping("/")
-  public void create(@RequestBody User user) {
-    System.out.println(user.getName());
+  @Autowired
+  private IUserRepository userRepository;
 
+  @PostMapping("/")
+  public ResponseEntity create(@RequestBody User responseUser) {
+    var user = this.userRepository.findByUsername(responseUser.getUsername());
+
+    if (user != null) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+    } else {
+      return ResponseEntity.status(HttpStatus.CREATED).body(this.userRepository.save(responseUser));
+    }
   }
 }
